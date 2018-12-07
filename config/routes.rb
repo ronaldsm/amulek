a8b982eabd99326ae3aad4b1985f57dce818c8ff
ronaldsm <ronaldsm@cisco.com>
2018-12-06 23:51:59 -0700

    db updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'companionship/index'
+
+  resources :companionships
+
+  root 'companionship#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
